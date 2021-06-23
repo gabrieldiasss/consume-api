0c8a8606a00e86969491262e3f311924f4511259
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2021-06-22 23:49:28 -0300

    style consume api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	return(
+		<div className="app">
+
+			<div className="cards">
+
+				<div className="card" >
+					<div className="card-body" >
+						<h1>Título do meu post</h1>
+						<div className="line"></div>
+						<h2>Conteudo da minha postagem, conteúdo da minha postagem</h2>
+					</div>
+				</div>
+			
+			</div>
+
+		</div>
+	)
+
 }
 
 export default App;
+
